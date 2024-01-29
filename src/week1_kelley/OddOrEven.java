ec6ac1191342e41d8f0bc4222b09066bb2964a73
kelley <kelleykimebrown@gmail.com>
2024-01-29 12:55:37 -0500

    update OddOrEven class

--- src/week1_kelley/OddOrEven.java
+++ src/week1_kelley/OddOrEven.java
@@ -1,4 +1,13 @@
 package week1_kelley;
 
 public class OddOrEven {
+
+    public static void identify(int num){
+        if(num % 2 == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+
 }
