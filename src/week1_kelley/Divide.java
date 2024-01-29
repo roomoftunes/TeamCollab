26b4b1d8550445723f6f50113edf70b5ffc4af15
kelley <kelleykimebrown@gmail.com>
2024-01-29 12:58:18 -0500

    update Divide class

--- src/week1_kelley/Divide.java
+++ src/week1_kelley/Divide.java
@@ -1,4 +1,16 @@
 package week1_kelley;
 
 public class Divide {
+
+    public static int divide(int num1, int num2) {
+
+        int result = 0;
+
+        while (num1 > 0){
+            num1 -= num2;
+            result++;
+        }
+        return result;
+    }
+
 }
