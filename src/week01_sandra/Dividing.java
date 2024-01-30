a25acc365bb69d21fa2ea644e5887ce939242e82
SandraVujacic <sandra120701@yahoo.com>
2024-01-30 14:26:26 -0500

    Create Dividing class and divideWithoutOperator method

---
+++ src/week01_sandra/Dividing.java
@@ -0,0 +1,17 @@
+package week01_sandra;
+
+public class Dividing {
+
+    public static void divideWithoutOperator(int num1, int num2){
+
+        int count = 0;
+
+        while (num1>=num2){
+            num1 -= num2;
+            count++;
+        }
+        System.out.println(count + " with a remainder of " + num1);
+    }
+}
+
+
