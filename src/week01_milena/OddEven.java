cb97816be53187b18f36741c37cec732867a21e2
MilenaCecez <milenacecez@gmail.com>
2024-01-30 12:35:43 -0500

    Create oddEven method

---
+++ src/week01_milena/OddEven.java
@@ -0,0 +1,17 @@
+package week01_milena;
+
+public class OddEven {
+
+
+    public static void oddEven(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + " is even");
+        } else {
+            System.out.println(num + " is odd");
+        }
+
+
+    }
+
+}
+
