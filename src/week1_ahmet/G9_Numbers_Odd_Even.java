48d00f570d36fcb00ea33bb2ee5682fff4a7d937
Matriks50 <ahmetsalesforce@gmail.com>
2024-02-01 10:53:27 -0500

    OddEVen method added and tested

---
+++ src/week1_ahmet/G9_Numbers_Odd_Even.java
@@ -0,0 +1,19 @@
+package week1_ahmet;
+
+public class G9_Numbers_Odd_Even {
+    public static void main(String[] args) {
+        int n = 5;
+        oddEven(n);
+    }
+
+    public static void oddEven(int num){
+        if( num % 2 == 0 ){
+            System.out.println("Even number");
+            }else{
+            System.out.println("Odd number");
+            }
+        }
+    }
+
+
+
