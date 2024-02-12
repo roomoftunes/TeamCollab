77743d87d6df2221ec393d94286745bf28c99b67
Milenacecez123 <milenacecez@gmail.com>
2024-02-11 20:40:31 -0500

    create class SwapNumbers

---
+++ src/week02_milena/SwapNumbers.java
@@ -0,0 +1,20 @@
+package week02_milena;
+
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        int num1 = 6;
+        int num2 = 14;
+
+        num1 = num1+num2;  // 20
+        num2 = num1-num2;  // 6
+        num1 = num1-num2;  // 14
+
+        System.out.println("Value of num1: " + num1);
+
+        System.out.println("Value of num2: " + num2);
+    }
+
+
+}
