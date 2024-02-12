98f3b282e194546c1098a803faac63dc5c8b437e
Matriks50 <ahmetsalesforce@gmail.com>
2024-02-12 14:12:33 -0500

    week2 DivisibleByThree_Five_Fifteen code was formatted

--- src/Week2_ahmet/DivisibleByThree_Five_Fifteen.java
+++ src/Week2_ahmet/DivisibleByThree_Five_Fifteen.java
@@ -3,22 +3,21 @@ package Week2_ahmet;
 public class DivisibleByThree_Five_Fifteen {
     public static void main(String[] args) {
 
-       int k;
+        int k;
         System.out.print("divisible by 3 :");
-
-        for (k = 1; k<= 100; k++) {
+        for (k = 1; k <= 100; k++) {
             if (k % 15 == 0) {
                 System.out.print(k + ", ");
             }
         }
 
-            int l;
-            System.out.print("\ndivisible by 5 :");
-            for (l = 1; l <= 100; l++) {
-                if (l % 5 == 0) {
-                    System.out.print(l + ", ");
-                }
+        int l;
+        System.out.print("\ndivisible by 5 :");
+        for (l = 1; l <= 100; l++) {
+            if (l % 5 == 0) {
+                System.out.print(l + ", ");
             }
+        }
 
         int m;
         System.out.print("\ndivisible by 3 :");
