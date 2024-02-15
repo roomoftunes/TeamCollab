8561f4aa085f94952ae139a81f55b02a1b2e7931
lussadecembre <lussa29@mail.com>
2024-02-15 02:00:15 -0500

    added week 3

---
+++ src/Lussa_week3/TaskWeek3.java
@@ -0,0 +1,10 @@
+package Lussa_week3;
+
+public class TaskWeek3 {
+}
+//1. Numbers -- Prime Number
+//Write a method that can check if a number is prime or not
+
+
+//2. Numbers -- Reverse negative number
+//Write a return method that can reverse negative number and return it as int
