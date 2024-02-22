ab4db0dc5d92264726f9ba2c5f700ab09a75e864
sandra120701 <sandra120701@yahoo.com>
2024-02-22 09:13:51 -0500

    rename isPrime method

--- src/week03_sandra/PrimeNumbers.java
+++ src/week03_sandra/PrimeNumbers.java
@@ -2,13 +2,21 @@ package week03_sandra;
 
 public class PrimeNumbers {
 
-    public static boolean isPrime(int num) {
+    public static void main(String[] args) {
+        System.out.println(isPrime(-32));
+    }
 
+    public static boolean isPrime(int num) {
+        if(num<0){
+            throw new RuntimeException("Please enter positive number");
+        }
         for (int i = 2; i < num; i++) {
             if (num % i == 0) {
                 return false;
             }
         }
+
+
         return true;
     }
 }
